55be52c2aacd579c7d513b6b106a207a3f72c53d
Christian Lizardi <cal352@cornell.edu>
2020-12-26 20:15:15 -0500

    Initial Commit

---
+++ Instant Instructor Team/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instant Instructor Team
+//
+//  Created by Christian Lizardi on 12/26/20.
+//  Copyright © 2020 Learn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
